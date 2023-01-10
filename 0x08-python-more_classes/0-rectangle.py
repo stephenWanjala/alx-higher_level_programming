dff7737f627fc388fa2d113db21fff8ead583873
wanjala stephen <stephenwanjala145@gmail.com>
2023-01-10 16:03:51 +0300

    add readme

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 0-rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
+# wanjala stephen <stephenwanjala145@gmail.com>
 """Defines a Rectangle class."""
 
 
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 1-rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
+# wanjala stephen <stephenwanjala145@gmail.com>
 """Defines a Rectangle class."""
 
 
