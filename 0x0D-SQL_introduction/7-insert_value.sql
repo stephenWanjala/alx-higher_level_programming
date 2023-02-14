cb420f814ebb8fe76d38dfe3905027fcc2b767f5
wanjala stephen <stephenwanjala145@gmail.com>
2023-02-14 16:16:42 +0300

    correct syntaxt

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,6 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-  --  New row:        id = 89 name = Best School
+
+-- Inserts a new row in a table
+-- Query to insert a new row in the table first_table
 INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+
 
